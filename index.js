b1c97dc8c6254093fe280a524255fe9818302c34
Duncan Gibson <legowerewolf@gmail.com>
2019-01-10 14:14:37 -0800

    Add support for module subpaths, ie "ipfs/src/core" in imports

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 let readFileSync = require('fs').readFileSync;
 let replace = require('gulp-replace');
 
-module.exports = () => replace(/import.*['"](.*)['"];/, (match) => {
-    let pkgname = match.match(/['"](.*)['"]/)[1];
+const importRegex = /import.*['"]([^/]*)(.*)['"];/
+
+module.exports = () => replace(importRegex, (match) => {
+    let pkgname = match.match(importRegex)[1];
     let pkgver = JSON.parse(readFileSync("./package.json")).dependencies[pkgname];
+    let pkgSubpath = match.match(importRegex)[2];
 
     if (!['.', '/'].includes(pkgname[0])) {
-        return match.replace(new RegExp(`${pkgname}`), `https://unpkg.com/${pkgname}@${pkgver}?module`);
+        return match.replace(new RegExp(`${pkgname}`), `https://unpkg.com/${pkgname}@${pkgver}${pkgSubpath}?module`);
     }
 
     return match;
