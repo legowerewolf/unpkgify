7a8abfd4b909b61d8b8e3d01d363e0af6c8b9e5f
Duncan Gibson <legowerewolf@gmail.com>
2019-01-10 14:29:20 -0800

    Fix a bug in the translation of subpathed module imports.

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ let replace = require('gulp-replace');
 const importRegex = /import.*['"]([^/]*)(.*)['"];/
 
 module.exports = () => replace(importRegex, (match) => {
-    let pkgname = match.match(importRegex)[1];
-    let pkgver = JSON.parse(readFileSync("./package.json")).dependencies[pkgname];
-    let pkgSubpath = match.match(importRegex)[2];
+
+
+    let fullImport = match.match(importRegex)[1];
+    let packageName = match.match(importRegex)[2];
+    let packageSubpath = match.match(importRegex)[3];
+
+    let packageVersion = JSON.parse(readFileSync("./package.json")).dependencies[pkgname];
 
     if (!['.', '/'].includes(pkgname[0])) {
-        return match.replace(new RegExp(`${pkgname}`), `https://unpkg.com/${pkgname}@${pkgver}${pkgSubpath}?module`);
+        return match.replace(new RegExp(`${fullImport}`), `https://unpkg.com/${packageName}@${packageVersion}${packageSubpath}?module`);
     }
 
     return match;
